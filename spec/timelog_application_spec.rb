f65deb93b4033dbedebd07d3a8a8df4753d40fb2
Alistair McKinnell <alistairm@nulogy.com>
2015-11-13 05:40:40 -0500

    Simplifies.
    Move the conversion of the hash options to an OpenStruct into the #log and #report methods.

--- spec/timelog_application_spec.rb
+++ spec/timelog_application_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe TimelogApplication do
   before do
     File.delete(TIMELOG_FILE_NAME) if File.exist?(TIMELOG_FILE_NAME)
 
-    log(options(user: "fred", project: "project-1", hours: 6.0))
-    log(options(user: "jim", project: "project-1", hours: 7.0))
-    log(options(user: "alice", project: "project-1", hours: 4.5))
+    log(user: "fred", project: "project-1", hours: 6.0)
+    log(user: "jim", project: "project-1", hours: 7.0)
+    log(user: "alice", project: "project-1", hours: 4.5)
   end
 
   after do
@@ -17,57 +17,43 @@ RSpec.describe TimelogApplication do
   end
 
   it "project total" do
-    reporting_options = options(project: "project-1")
-
-    result = report(reporting_options).split("\n")[-1]
+    result = report(project: "project-1").split("\n")[-1]
 
     expect(result.split[1].to_f).to eq(17.5)
   end
 
   it "project total for missing project" do
-    reporting_options = options(project: "project-2")
-
-    result = report(reporting_options).split("\n")[-1]
+    result = report(project: "project-2").split("\n")[-1]
 
     expect(result.split[1].to_f).to eq(0)
   end
 
   it "user total" do
-    reporting_options = options(user: "fred", project: "project-1")
-
-    result = report(reporting_options).split("\n")[-1]
+    result = report(user: "fred", project: "project-1").split("\n")[-1]
 
     expect(result.split[1].to_f).to eq(6)
   end
 
   it "user total for missing user" do
-    reporting_options = options(user: "harry", project: "project-1")
-
-    result = report(reporting_options).split("\n")[-1]
+    result = report(user: "harry", project: "project-1").split("\n")[-1]
 
     expect(result.split[1].to_f).to eq(0)
   end
 
   it "user total for missing project" do
-    reporting_options = options(user: "fred", project: "project-2")
-
-    result = report(reporting_options).split("\n")[-1]
+    result = report(user: "fred", project: "project-2").split("\n")[-1]
 
     expect(result.split[1].to_f).to eq(0)
   end
 
   def log(logging_options)
     timelog_application = TimelogApplication.new(TIMELOG_FILE_NAME)
-    timelog_application.log(logging_options)
+    timelog_application.log(OpenStruct.new(logging_options))
   end
 
   def report(reporting_options)
     timelog_application = TimelogApplication.new(TIMELOG_FILE_NAME)
-    timelog_application.report(reporting_options)
-  end
-
-  def options(opts)
-    OpenStruct.new(opts)
+    timelog_application.report(OpenStruct.new(reporting_options))
   end
 
 end
